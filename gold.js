a2da78afa9517c12e9c5bc327d4cb6516167f317
AndrewsGilbert <tandrewsgilbert@gmail.com>
2021-07-05 23:22:49 +0530

    2nd rev

--- gold.js
+++ gold.js
@@ -3,113 +3,78 @@ const fs = require('fs')
 const request = require('request')
 const cheerio = require('cheerio')
 const app = express()
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
 const cron = require('node-cron')
 
-const sender = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'andrewsgilbert95@gmail.com',
-    pass: ''
+cron.schedule('* 9-15 * * *', function () {
+  const calender = new Date()
+  const hour = calender.getHours()
+  const date = calender.toDateString()
+  const time = calender.toTimeString()
+
+  const content = fs.readFileSync('gold.json', 'utf8')
+  const contentJson = JSON.parse(content)
+  const web = contentJson.web
+  const goldObject = contentJson.goldRate
+  let webId = ''
+  let goldRate = ''
+  const detail = {}
+
+  for (let i = 0; i < web.length; i++) {
+    const id = web[i].id
+    const url = web[i].url
+
+    if (id === 1 && (hour === 9 || hour === 12 || hour === 15)) {
+      webId = id
+      request(url, function (err, resp, html) {
+        if (!err && resp.statusCode === 200) {
+          const $ = cheerio.load(html)
+          const dataPath = $('.rate')
+          const data = dataPath.text()
+          goldRate = Number(data.replace(/,/g, '')) / 10
+          process()
+        }
+      })
+    }
+
+    if (id === 2 && (hour === 10 || hour === 14)) {
+      webId = id
+      request(url, function (err, resp, html) {
+        if (!err && resp.statusCode === 200) {
+          const $ = cheerio.load(html)
+          const dataPath = $('._cptblwrp table tbody tr:nth-child(3) td._lft')
+          const data = dataPath.html()
+          goldRate = Number(data.replace(/₹|,/g, '')) / 10
+          process()
+        }
+      })
+    }
+
+    if (id === 3 && hour === 13) {
+      webId = id
+      request(url, function (err, resp, html) {
+        if (!err && resp.statusCode === 200) {
+          const $ = cheerio.load(html)
+          const dataPath = $('.commodityInfoContainer > ul > li.commodityPriceCol > span.commodityPrice')
+          const data = dataPath.html()
+          goldRate = Number(data) / 10
+          process()
+        }
+      })
+    }
+
+    function process () {
+      detail.Rate = goldRate
+      detail.DAte = date
+      detail.Time = time
+      detail.WebId = webId
+      const index = goldObject.length
+
+      goldObject[index] = detail
+
+      fs.writeFileSync('gold.json', JSON.stringify(contentJson, null, 2), 'utf8')
+    }
   }
-})
-
-
-
-cron.schedule('35,36,37 9,16 * * *', function () {
-
-    const calender = new Date()
-    const hour = calender.getHours()
-    const min = calender.getMinutes()
-    const date = calender.getDate()
-    const month = calender.getMonth()
-    const year = calender.getFullYear()
-
-    const urlContent =  fs.readFileSync('web.json', 'utf8')
-    const urlObject = JSON.parse(urlContent)
-    const urlWeb = Object.keys(urlObject)
-    const urlCount = urlWeb.length
-    var detail = {}
-    var childId = {}
-    var goldRate =''
-    var webId =''
-
-    for ( web in urlObject ) {
-
-        if( web === 'goldpricesindia' && min === 35) {
-
-            const url = urlObject[web]
-            webId = web
-
-            request(url, function (err, resp, html) { 
-                if (!err && resp.statusCode === 200) {
-                    const $ = cheerio.load(html)
-                    const dataPath = $('.rate')
-                    const data = dataPath.text()
-                    goldRate = Number(data.replace(/,/g, '')) / 10
-                    process()
-                }
-           })        
-        }
-        if( web === 'gadgets' && min === 36) {
-
-            const url = urlObject[web]
-            webId = web
-
-            request(url, function (err, resp, html) {
-                if (!err && resp.statusCode === 200) {
-                    const $ = cheerio.load(html)
-                    const dataPath = $('._cptblwrp table tbody tr:nth-child(3) td._lft')
-                    const data = dataPath.html()
-                    goldRate = Number(data.replace(/₹|,/g, '')) / 10
-                    process()
-                }
-            })
-        }
-        if( web === 'economictimes' && min === 37) {
-
-            const url = urlObject[web]
-            webId = web
-
-            request(url, function (err, resp, html) {
-                if (!err && resp.statusCode === 200) {
-                    const $ = cheerio.load(html)
-                    const dataPath = $('.commodityInfoContainer > ul > li.commodityPriceCol > span.commodityPrice')
-                    const data = dataPath.html()
-                     goldRate = Number(data) / 10
-                     process()
-                }
-
-            })
-        }
-        function process(){
-
-            detail["Rate"] = goldRate
-            detail["Date"] = date
-            detail["Time"] = hour
-            detail["Min"] = min
-            detail["Month"] = month
-            detail["year"] = year
-
-            childId[webId] = detail
-
-            const GoldContent = fs.readFileSync('gold.json', 'utf8')
-            const goldObject = JSON.parse(GoldContent)
-            const keys = Object.keys(goldObject)
-            const keysLength = keys.length
-            const index = urlWeb.indexOf(webId)
-            var nextKey = ''
-                    
-            if ( index === 0 && hour == 9 ) {
-                nextKey = keysLength + 1
-                goldObject[nextKey] = childId
-            } else {
-                nextKey = keysLength
-                goldObject[nextKey][webId] = detail
-            }   
-            fs.writeFileSync('gold.json', JSON.stringify(goldObject, null, 2), 'utf8')
-        }
-    }    
 })
 
 app.listen(8586, function () {
